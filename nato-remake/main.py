d6ab5c9811a16975af9d467e8aaf9ca752380708
Mirko Ruhl <107079974+RuhlMirko@users.noreply.github.com>
2023-07-21 22:52:11 -0300

    Add files via upload

---
+++ nato-remake/main.py
@@ -0,0 +1,19 @@
+# Keyword Method with iterrows()
+# {new_key:new_value for (index, row) in df.iterrows()}
+
+import pandas
+
+data = pandas.read_csv("nato_phonetic_alphabet.csv")
+phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
+print(phonetic_dict)
+
+ok = True
+while ok:
+    try:
+        word = input("Enter a word: ").upper()
+        output_list = [phonetic_dict[letter] for letter in word]
+        print(output_list)
+        ok = False
+    except KeyError:
+        print("Sorry, only letters in the alphabet please")
+
